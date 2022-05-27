a0e07a7942f0ed508cf0eab568d677ed800c55df
Hfeno <rhharifeno@gmail.com>
2022-05-27 12:21:43 +0200

    first commit

--- src/main/java/com/annonce/SpringSecurityConfig/SecurityConstants.java
+++ src/main/java/com/annonce/SpringSecurityConfig/SecurityConstants.java
@@ -1,7 +1,7 @@
 package com.annonce.SpringSecurityConfig;
 
 public class SecurityConstants {
-	public static final String SECRET ="rhh@gmail.com";
+	public static final String SECRET ="0000";
 	public static final long EXPIRATION_TIME= 864_000_000;//10 jours
 	public static final String TOKEN_PREFIX ="Bearer ";
 	public static final String HEADER_STRING= "Authorization";
